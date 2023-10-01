10dd729080de746a1ae36dd7f0b4d39f0da624e6
bransome <bransometan@gmail.com>
2023-10-01 16:48:05 +0800

    Table Component, My Property Table, less buttons

---
+++ src/components/Table.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="searchBar">
+    <!-- Filter Search -->
+    <div class="input-group mb-3">
+      <input
+        type="search"
+        class="form-control"
+        v-model="searchQuery"
+        placeholder="Search "
+      />
+    </div>
+  </div>
+
+  <table id="tableComponent" class="table table-bordered table-striped">
+    <thead>
+      <tr>
+        <!-- loop through each value of the fields to get the table header -->
+        <th v-for="field in fields" :key="field" @click="sortTable(field)">
+          {{ field }}
+          <span class="material-symbols-outlined icon-align"> swap_vert </span>
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <!-- Loop through the list get the each student data -->
+      <tr v-for="item in filteredList" :key="item">
+        <td v-for="field in fields" :key="field">
+          <template v-if="field === 'Actions'">
+            <!-- Append child element here if field is "Actions" -->
+            <div class="text-center" v-html="item[field]"></div>
+          </template>
+
+          <!-- Display item[field] for other fields -->
+          <template v-else>
+            {{ item[field] }}
+          </template>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+  <script>
+import { computed, ref } from "vue";
+// Importing  the lodash library
+import { sortBy } from "lodash";
+
+export default {
+  name: "TableComponent",
+  props: {
+    Data: {
+      type: Array,
+    },
+    fields: {
+      type: Array,
+    },
+  },
+  methods: {
+    parseHTML(htmlString) {
+      const parser = new DOMParser();
+      // Parse the HTML string into a new HTML document
+      const htmlDocument = parser.parseFromString(htmlString, "text/html");
+      const element = htmlDocument.querySelector("button");
+
+      return element;
+    },
+    renderButtons() {
+      for (const item of this.filteredList) {
+        for (const field of this.fields) {
+          if (field === "Actions") {
+            console.log(item[field]);
+            conso;
+
+            // Append the button element to the <td>
+            //element.appendChild(this.parseHTML(item[field]));
+          }
+        }
+      }
+    },
+  },
+
+  setup(props) {
+    let sort = ref(false);
+    let updatedList = ref([]);
+    let searchQuery = ref("");
+
+    // a function to sort the table
+    const sortTable = (col) => {
+      sort.value = !sort.value;
+      // Use of _.sortBy() method
+      updatedList.value = sortBy(props.Data, col);
+    };
+
+    const sortedList = computed(() => {
+      if (sort.value) {
+        return updatedList.value;
+      } else {
+        return props.Data;
+      }
+    });
+
+    // Filter Search
+    const filteredList = computed(() => {
+      return sortedList.value.filter((product) => {
+        const searchQueryLower = searchQuery.value.toLowerCase();
+        for (const key in product) {
+          if (product.hasOwnProperty(key)) {
+            const propertyValue = product[key].toLowerCase();
+            if (propertyValue.indexOf(searchQueryLower) !== -1) {
+              return true;
+            }
+          }
+        }
+        return false;
+      });
+    });
+
+    return { sortedList, sortTable, searchQuery, filteredList };
+  },
+};
+</script>
+  
+<style scoped>
+table th:hover {
+  background: #f2f2f2;
+}
+
+.icon-align {
+  vertical-align: text-bottom;
+}
+</style>
--- src/views/MyProperty.vue
+++ src/views/MyProperty.vue
@@ -1,12 +1,230 @@
 <template>
   <main class="myproperty-page">
     <h1>My Property</h1>
-    <p>This is the My Property page</p>
+    <!-- Add Property component -->
+    <AddProperty @addedProperty="change" />
+    <div
+      class="overflow-auto p-3"
+      style="min-height: 200px; max-height: 400px; width: 100%"
+    >
+      <!-- The Property Table component -->
+      <Table
+        :key="refreshComp"
+        :fields="Propertyfields"
+        :Data="PropertyData"
+      ></Table>
+      <h5 class="text-center" v-if="PropertyData.length == 0">
+        There are currently no records available
+      </h5>
+    </div>
+    <h1 class="my-5">Manage Rental</h1>
+    <div
+      class="overflow-auto p-3"
+      style="min-height: 200px; max-height: 400px; width: 100%"
+    >
+      <!-- The Manage Rental Table component -->
+      <Table
+        :key="refreshComp"
+        :fields="Tenantfields"
+        :Data="TenantData"
+      ></Table>
+      <h5 class="text-center" v-if="TenantData.length == 0">
+        There are currently no records available
+      </h5>
+    </div>
   </main>
 </template>
   
-  <script>
-export default {};
+<script>
+// Importing the table component
+import Table from "../components/Table.vue";
+import AddProperty from "../components/AddProperty.vue";
+import { auth, db } from "../firebase.js";
+
+import {
+  collection,
+  getDocs,
+  doc,
+  query,
+  where,
+  updateDoc,
+  deleteDoc,
+  orderBy,
+} from "firebase/firestore";
+
+export default {
+  name: "App",
+  components: {
+    Table,
+    AddProperty,
+  },
+  data() {
+    return {
+      refreshComp: 0,
+      PropertyData: [],
+      Propertyfields: [
+        "ID",
+        "Property_Type",
+        "Postal_Code",
+        "Address",
+        "Unit_Number",
+        "Purchase_Price",
+        "Upfront_Payment",
+        "Actions",
+      ],
+      TenantData: [],
+      Tenantfields: [
+        "ID",
+        "Tenant_Name",
+        "Monthly_Rent",
+        "Contract_Start_Date",
+        "Contract_End_Date",
+        "Unpaid_Months",
+        "Next_Payment_Date",
+        "Actions",
+      ],
+    };
+  },
+  methods: {
+    change() {
+      this.refreshComp += 1;
+      console.log("Refreshed!");
+      auth.onAuthStateChanged((newUserState) => {
+        let user = newUserState;
+        this.displayProperty(user).then((result) => {
+          this.PropertyData = result;
+        });
+        this.displayTenant(user).then((result) => {
+          this.TenantData = result;
+        });
+      });
+    },
+    async displayProperty(user) {
+      let q = query(
+        collection(db, "Property"),
+        where("userId", "==", user.uid),
+        orderBy("timestamp", "desc")
+      );
+      //console.log(auth.currentUser.uid);
+      const querySnapshot = await getDocs(q);
+      let index = 1;
+      let propertyData = [];
+
+      querySnapshot.forEach((docs) => {
+        let documentData = docs.data();
+
+        let propertyType = documentData.isOwnerOccupied;
+        let postalCode = documentData.postalCode;
+        let fullAddress = documentData.address;
+        let unitNumber = documentData.unitNumber;
+        let purchasePrice = documentData.purchasePrice;
+        let upfrontPayment = documentData.upfrontPayment;
+
+        let viewButton =
+          '<button type="button" id="vp_' +
+          index +
+          '" class="btn btn-primary mb-3">View Tenant Transactions</button>';
+        let manageButton =
+          '<button type="button" id="mp_' +
+          index +
+          '" class="btn btn-primary">Manage Property</button>';
+
+        if (propertyType == "0") {
+          propertyData.push({
+            ID: index.toString(),
+            Property_Type:
+              propertyType == "0" ? "Owner Occupied" : "Rental/Investment",
+            Postal_Code: postalCode,
+            Address: fullAddress,
+            Unit_Number: "#" + unitNumber,
+            Purchase_Price: "$" + purchasePrice,
+            Upfront_Payment: "$" + upfrontPayment,
+            Actions: manageButton,
+          });
+        } else {
+          propertyData.push({
+            ID: index.toString(),
+            Property_Type:
+              propertyType == "0" ? "Owner Occupied" : "Rental/Investment",
+            Postal_Code: postalCode,
+            Address: fullAddress,
+            Unit_Number: "#" + unitNumber,
+            Purchase_Price: "$" + purchasePrice,
+            Upfront_Payment: "$" + upfrontPayment,
+            Actions: viewButton + "<br/>" + manageButton,
+          });
+        }
+
+        index += 1;
+      });
+
+      //console.log(JSON.stringify(propertyData));
+      return propertyData;
+    },
+    async displayTenant(user) {
+      let q = query(
+        collection(db, "Tenant"),
+        where("userId", "==", user.uid),
+        orderBy("timestamp", "desc")
+      );
+      //console.log(auth.currentUser.uid);
+      const querySnapshot = await getDocs(q);
+      let index = 1;
+      let TenantData = [];
+
+      querySnapshot.forEach((docs) => {
+        let documentData = docs.data();
+
+        let firstName = documentData.firstName;
+        let lastName = documentData.lastName;
+        let fullName = firstName + " " + lastName;
+        let monthlyRent = documentData.monthlyRent.toString();
+        let contractStartDate = documentData.contractStartDate.toString();
+        let contractEndDate = documentData.contractEndDate.toString();
+        let unpaidMonths = documentData.unpaidMonths.toString();
+        let nextPaymentDate =
+          documentData.nextPaymentDate == undefined
+            ? ""
+            : new Date(documentData.nextPaymentDate.toDate())
+                .toISOString()
+                .split("T")[0]
+                .toString();
+
+        let manageButton =
+          '<button type="button" id="mt_' +
+          index +
+          '" class="btn btn-primary">Manage Payment</button>';
+
+        TenantData.push({
+          ID: index.toString(),
+          Tenant_Name: fullName,
+          Monthly_Rent: "$" + monthlyRent,
+          Contract_Start_Date: contractStartDate,
+          Contract_End_Date: contractEndDate,
+          Unpaid_Months: unpaidMonths,
+          Next_Payment_Date: nextPaymentDate,
+          Actions: manageButton,
+        });
+
+        index += 1;
+      });
+
+      //console.log(JSON.stringify(propertyData));
+      return TenantData;
+    },
+  },
+  mounted() {
+    auth.onAuthStateChanged((newUserState) => {
+      let user = newUserState;
+      this.displayProperty(user).then((result) => {
+        this.PropertyData = result;
+      });
+      this.displayTenant(user).then((result) => {
+        this.TenantData = result;
+      });
+    });
+  },
+};
 </script>
   
   <style>
